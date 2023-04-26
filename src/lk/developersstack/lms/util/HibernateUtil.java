df7d8ef5e2224f9dee09af7851050783a3f9a404
ozen-hub <info.ozenhub@gmail.com>
2023-04-26 23:32:56 +0530

    22

--- src/lk/developersstack/lms/util/HibernateUtil.java
+++ src/lk/developersstack/lms/util/HibernateUtil.java
@@ -8,38 +8,56 @@ import org.hibernate.boot.MetadataSources;
 import org.hibernate.boot.model.naming.ImplicitNamingStrategyJpaCompliantImpl;
 import org.hibernate.boot.registry.StandardServiceRegistry;
 import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+import org.hibernate.cfg.Configuration;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.Properties;
 
 public class HibernateUtil {
     private static HibernateUtil hibernateUtil;
-    private final static SessionFactory sessionFactory=buildSessionFactory();
+    private final static SessionFactory sessionFactory = buildSessionFactory();
 
-    private HibernateUtil(){}
-    public static HibernateUtil getInstance(){
-        return (hibernateUtil==null?hibernateUtil=new HibernateUtil():hibernateUtil);
+    private HibernateUtil() {
     }
 
-    private static SessionFactory buildSessionFactory(){
-        StandardServiceRegistry standardRegistry = new StandardServiceRegistryBuilder()
+    public static HibernateUtil getInstance() {
+        return (hibernateUtil == null ? hibernateUtil = new HibernateUtil() : hibernateUtil);
+    }
+
+    private static SessionFactory buildSessionFactory() {
+        Properties properties = new Properties();
+        try {
+            properties.load(new FileInputStream("hibernate.properties"));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        Configuration configuration = new Configuration();
+        configuration.addProperties(properties);
+        configuration.addAnnotatedClass(Student.class)
+                .addAnnotatedClass(Program.class)
+                .addAnnotatedClass(Laptop.class)
+                .addAnnotatedClass(Book.class)
+                .addAnnotatedClass(Registration.class);
+        return configuration.buildSessionFactory();
+
+        /*StandardServiceRegistry standardRegistry = new StandardServiceRegistryBuilder()
                 .configure("hibernate.cfg.xml")
                 .build();
 
         Metadata metadata = new MetadataSources(standardRegistry)
-                .addAnnotatedClass(Student.class)
-                .addAnnotatedClass(Program.class)
-                .addAnnotatedClass(Laptop.class)
-                .addAnnotatedClass(Book.class)
-                .addAnnotatedClass(Registration.class)
+
                 .getMetadataBuilder()
                 .applyImplicitNamingStrategy(ImplicitNamingStrategyJpaCompliantImpl.INSTANCE)
                 .build();
 
         return metadata.getSessionFactoryBuilder()
-                .build();
+                .build();*/
 
     }
 
-    public Session openSession(){
-       return sessionFactory.openSession();
+    public Session openSession() {
+        return sessionFactory.openSession();
     }
 
 }
